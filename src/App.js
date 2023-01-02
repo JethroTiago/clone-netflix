3be6af686259a229cbb8595d5fb1ac4ba955c606
Jethro <jethrotiago@hotmail.com>
2023-01-01 23:00:14 -0300

    Start loading movies lists

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Tmdb from "./Tmdb";
 
 export default () => {
+
+  useEffect(() => {
+    const loadAll = async () => {
+      //Taking the total list
+      let list = await Tmdb.getHomeList();
+      console.log(list);
+    }
+
+    loadAll();
+  }, []);
+
   return (
     <div>
       <h1>DeadfliX</h1>
--- src/Tmdb.js
+++ src/Tmdb.js
@@ -12,48 +12,54 @@ const API_BASE = 'https://api.themoviedb.org/3';
 - Cósmico
 */
 
+const basicFetch = async (endpoint) => {
+    const req = await fetch(`${API_BASE}${endpoint}`);
+    const json = await req.json();
+    return json;
+}
+
 export default {
     getHomeList: async () => {
         return [
             {
-                slug: 'best',
+                slug: 'toprated',
                 title: 'Melhores filmes',
-                items: []
+                items: await basicFetch(`/discover/tv?with_network=213&language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'supernatural',
                 title: 'Sobrenatural',
-                items: []
+                items: await basicFetch(`/trending/all/week?language=pt=BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'gore',
                 title: 'Gore',
-                items: []
+                items: await basicFetch(`/movie/top_rated?language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'classics',
                 title: 'Clássicos',
-                items: []
+                items: await basicFetch(`/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'documentary',
                 title: 'Documentário',
-                items: []
+                items: await basicFetch(`/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`)
             },
             {
-                slug: 'ffootage',
+                slug: 'footage',
                 title: 'Found Footage',
-                items: []
+                items: await basicFetch(`/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'trash',
                 title: 'Trash',
-                items: []
+                items: await basicFetch(`/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'cosmic',
                 title: 'Cósmico',
-                items: []
+                items: await basicFetch(`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`)
             },
         ];
     }
