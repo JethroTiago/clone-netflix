63eb592a2c430f4b52b0c450095ad526aca14df9
Jethro <jethrotiago@hotmail.com>
2023-01-01 22:11:28 -0300

    Added Tmdb lists

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -1,2 +1,60 @@
 const API_KEY = 'da54db69cd70d0c038a1de3218ea9347';
 const API_BASE = 'https://api.themoviedb.org/3';
+
+/*
+- Melhores filmes
+- Sobrenatural
+- Gore
+- Clássicos
+- Documentários
+- Found Footage
+- Trash
+- Cósmico
+*/
+
+export default {
+    getHomeList: async () => {
+        return [
+            {
+                slug: 'best',
+                title: 'Melhores filmes',
+                items: []
+            },
+            {
+                slug: 'supernatural',
+                title: 'Sobrenatural',
+                items: []
+            },
+            {
+                slug: 'gore',
+                title: 'Gore',
+                items: []
+            },
+            {
+                slug: 'classics',
+                title: 'Clássicos',
+                items: []
+            },
+            {
+                slug: 'documentary',
+                title: 'Documentário',
+                items: []
+            },
+            {
+                slug: 'ffootage',
+                title: 'Found Footage',
+                items: []
+            },
+            {
+                slug: 'trash',
+                title: 'Trash',
+                items: []
+            },
+            {
+                slug: 'cosmic',
+                title: 'Cósmico',
+                items: []
+            },
+        ];
+    }
+}
