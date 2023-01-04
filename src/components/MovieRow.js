0e332eb1f6030c3b1596567f39d2223768c8871a
Jethro <jethrotiago@hotmail.com>
2023-01-04 19:45:11 -0300

    Starting movieRow list

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -6,10 +6,13 @@ export default ({title, items}) => {
         <div>
             <h2>{title}</h2>
             <div className="movieRow--listarea">
-                {items.results.length > 0 && items.results.map((item, key) => (
-                    <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
-                ))}
-
+                <div className='movieRow--list'>
+                    {items.results.length > 0 && items.results.map((item, key) => (
+                        <div key={key} className='movieRow--item'>
+                            <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
+                        </div>
+                    ))}  
+                </div>
             </div>
         </div>
     );
