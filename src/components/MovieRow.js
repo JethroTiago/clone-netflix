f2518f179f50e73276daf4dfcea6ca1e7ea8c6ce
Jethro <jethrotiago@hotmail.com>
2023-01-04 19:30:34 -0300

    Starting firsts styles

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -7,7 +7,7 @@ export default ({title, items}) => {
             <h2>{title}</h2>
             <div className="movieRow--listarea">
                 {items.results.length > 0 && items.results.map((item, key) => (
-                    <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} />
+                    <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
                 ))}
 
             </div>
