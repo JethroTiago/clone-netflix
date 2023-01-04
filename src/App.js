9fc2d0f48bfd562c0e3d9bf59f6baa3c1308d07a
Jethro <jethrotiago@hotmail.com>
2023-01-04 20:24:24 -0300

    Adding FeaturedMovie components to the app - first step

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from "react";
 import './App.css';
 import Tmdb from "./Tmdb";
 import MovieRow from "./components/MovieRow";
+import FeaturedMovie from "./components/FeaturedMovie";
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
 
   useEffect(() => {
     const loadAll = async () => {
@@ -19,6 +21,11 @@ export default () => {
 
   return (
     <div className="page">
+
+      {featuredData &&
+        <FeaturedMovie item ={featuredData} />
+      }
+
       <section className="lists">
         {movieList.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items} />
