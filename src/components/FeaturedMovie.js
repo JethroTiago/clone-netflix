c1a3b5156a209bf7e62b005b4b5bd945027509fc
Jethro <jethrotiago@hotmail.com>
2023-01-05 14:35:44 -0300

    Showing random movie original name and image on screen

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -3,7 +3,11 @@ import './FeaturedMovie.css';
 
 export default ({item}) => {
     return (
-        <section className='featured'>
+        <section className="featured" style={{
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
+        }}>
             <div>{item.original_name}</div>
         </section>
     );
