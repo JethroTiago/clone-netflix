972cccdbee92913857452d95dc9bb40ed04b3500
Jethro <jethrotiago@hotmail.com>
2023-01-07 22:41:32 -0300

    Added genres information

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -5,7 +5,11 @@ export default ({item}) => {
     console.log(item);
 
     let firstDate = new Date(item.first_air_date);
-
+    let genres = [];
+    for(let i in item.genres) {
+        genres.push( item.genres[i].name );
+    }
+    
     return (
         <section className="featured" style={{
             backgroundSize: 'cover',
@@ -24,7 +28,7 @@ export default ({item}) => {
                     <div className="featured--buttons">
 
                     </div>
-                    <div className="featured--genres"><strong>Gêneros:</strong></div>
+                    <div className="featured--genres"><strong>Gêneros:</strong> {genres.join(', ')}</div>
                 </div>
             </div>
         </section>
