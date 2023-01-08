3d7cb776c90d9830eb760cd77b784e2e0511e394
Jethro <jethrotiago@hotmail.com>
2023-01-07 22:48:22 -0300

    Added buttons

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -26,7 +26,8 @@ export default ({item}) => {
                     </div>
                     <div className="featured--description">{item.overview}</div>
                     <div className="featured--buttons">
-
+                        <a href={`/watch/${item.id}`}>▶ Assistir</a>
+                        <a href={`/list/add/${item.id}`}>+ Minha Lista</a>
                     </div>
                     <div className="featured--genres"><strong>Gêneros:</strong> {genres.join(', ')}</div>
                 </div>
