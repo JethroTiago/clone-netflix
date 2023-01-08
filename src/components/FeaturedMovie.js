2c83ee9d5f1b98f8a4399f368adce4014d88b118
Jethro <jethrotiago@hotmail.com>
2023-01-07 22:34:56 -0300

    Added release year

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -3,6 +3,9 @@ import './FeaturedMovie.css';
 
 export default ({item}) => {
     console.log(item);
+
+    let firstDate = new Date(item.first_air_date);
+
     return (
         <section className="featured" style={{
             backgroundSize: 'cover',
@@ -14,14 +17,14 @@ export default ({item}) => {
                     <div className="featured--name">{item.original_name}</div>
                     <div className="featured--info">
                         <div className="featured--points">Nota {item.vote_average}</div>
-                        <div className="featured--year">2023</div>
+                        <div className="featured--year">{firstDate.getFullYear()}</div>
                         <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
                     </div>
                     <div className="featured--description">{item.overview}</div>
                     <div className="featured--buttons">
 
                     </div>
-                    <div className="featured--genres"></div>
+                    <div className="featured--genres"><strong>Gêneros:</strong></div>
                 </div>
             </div>
         </section>
