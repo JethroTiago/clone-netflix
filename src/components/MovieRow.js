43b8383d462ef3885c325c17ec7ee4f7ee6ab2bb
Jethro <jethrotiago@hotmail.com>
2023-01-09 12:09:14 -0300

    Setting movie row to right and left

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './MovieRow.css';
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
 export default ({title, items}) => {
+    const [scrollX, setScrollX] = useState(-400);
+
+    const handleLeftArrow = () => {
+        let x = scrollX + Math.round(window.innerWidth / 2);
+        if(x > 0) {
+            x = 0;
+        };
+        setScrollX(x);
+    };
+
+    const handleRightArrow = () => {
+        let x = scrollX - Math.round(window.innerWidth / 2);
+        let listWidth = items.results.length * 150;
+        if((window.innerWidth - listWidth) > x) {
+            x = (window.innerWidth - listWidth) - 60;
+        };
+        setScrollX(x);
+    };
+
     return (
         <div className='movieRow'>
             <h2>{title}</h2>
-            <div className="movieRow--left">
+            <div className="movieRow--left" onClick={handleLeftArrow}>
                 <NavigateBeforeIcon style={{fontSize: 50}} />
             </div>
-            <div className="movieRow--right">
+            <div className="movieRow--right" onClick={handleRightArrow}>
                 <NavigateNextIcon style={{fontSize: 50}} />
             </div>
             <div className="movieRow--listarea">
-                <div className='movieRow--list'>
+                <div className="movieRow--list" style={{
+                    marginLeft: scrollX,
+                    width: items.results.length * 150
+                }}>
                     {items.results.length > 0 && items.results.map((item, key) => (
                         <div key={key} className='movieRow--item'>
                             <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
