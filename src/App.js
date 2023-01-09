1acc76a4f9d341d993becb216006999bd3b99b29
Jethro <jethrotiago@hotmail.com>
2023-01-08 22:04:38 -0300

    Seting header background transparency

--- src/App.js
+++ src/App.js
@@ -28,6 +28,22 @@ export default () => {
     loadAll();
   }, []);
 
+  useEffect(() => {
+    const scrollListener = () => {
+      if(window.scrollY > 10) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    }
+    window.addEventListener('scroll', scrollListener);
+
+    return () => {
+      window.removeEventListener('scroll', scrollListener);
+    }
+
+}, []);
+
   return (
     <div className="page">
 
