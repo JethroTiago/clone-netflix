3c686ce0c16c42bd605768e0542dc4a4e93b8514
Jethro <jethrotiago@hotmail.com>
2023-01-09 12:42:20 -0300

    Ending project

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -9,6 +9,11 @@ export default ({item}) => {
     for(let i in item.genres) {
         genres.push( item.genres[i].name );
     }
+
+    let description = item.overview;
+    if(description.length > 250) {
+        description = description.substring(0, 250)+"...";
+    }
     
     return (
         <section className="featured" style={{
@@ -24,7 +29,7 @@ export default ({item}) => {
                         <div className="featured--year">{firstDate.getFullYear()}</div>
                         <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
                     </div>
-                    <div className="featured--description">{item.overview}</div>
+                    <div className="featured--description">{description}</div>
                     <div className="featured--buttons">
                         <a href={`/watch/${item.id}`} className="featured--watchbutton">▶ Assistir</a>
                         <a href={`/list/add/${item.id}`} className="featured--mylistbutton">+ Minha Lista</a>
