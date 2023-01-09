54530fb20986fbbd56f1d9953500c9de0c44360b
Jethro <jethrotiago@hotmail.com>
2023-01-09 11:27:27 -0300

    Added navigate icons

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './MovieRow.css';
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
 export default ({title, items}) => {
     return (
         <div className='movieRow'>
             <h2>{title}</h2>
+            <div className="movieRow--left">
+                <NavigateBeforeIcon style={{fontSize: 50}} />
+            </div>
+            <div className="movieRow--right">
+                <NavigateNextIcon style={{fontSize: 50}} />
+            </div>
             <div className="movieRow--listarea">
                 <div className='movieRow--list'>
                     {items.results.length > 0 && items.results.map((item, key) => (
