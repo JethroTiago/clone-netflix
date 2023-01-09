6758534bd562e92fba91b55c9f22a433d660aefd
Jethro <jethrotiago@hotmail.com>
2023-01-09 12:33:17 -0300

    Added responsiveness for mobile devices

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -4,7 +4,7 @@ import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
 export default ({title, items}) => {
-    const [scrollX, setScrollX] = useState(-400);
+    const [scrollX, setScrollX] = useState(0);
 
     const handleLeftArrow = () => {
         let x = scrollX + Math.round(window.innerWidth / 2);
